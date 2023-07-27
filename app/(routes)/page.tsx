c5c05e3035dc53d36c5338bc7ead2fc6b46404c9
ayan471 <pakhiraayan@gmail.com>
2023-07-27 18:36:33 +0530

    creating the store

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,18 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("315f4f89-72d7-49ea-bfee-29024bf851b8");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
